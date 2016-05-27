0fe7119f0e6d441f23aaa45eac911a95e64ab994
vandjelk <vandjelkoski@gmail.com>
2016-05-27 16:37:46 +0200

    Gets the pricing code for child movies.

--- VideoStore/Price.cs
+++ VideoStore/Price.cs
@@ -21,7 +21,7 @@ namespace VideoStore
     public class ChildrensPrice : Price
     {
          /// <summary>
-         /// Gets the pricing code for childrens movies.
+         /// Gets the pricing code for child movies.
          /// </summary>
          /// <returns>Returns the value of the pricing code.</returns>
         public override int GetPriceCode()
